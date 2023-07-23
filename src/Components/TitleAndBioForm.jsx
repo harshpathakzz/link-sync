24d22cdffe402fbb6836af76025dd9b825136fcf
harshpathakzz <harshpathak2017@gmail.com>
2023-07-23 19:18:41 +0530

    added text limit

--- src/Components/TitleAndBioForm.jsx
+++ src/Components/TitleAndBioForm.jsx
@@ -2,6 +2,7 @@ import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { useTitleAndBioContext } from "../context/TitleAndBioContext";
 
 const TitleAndBioForm = () => {
@@ -22,7 +23,13 @@ const TitleAndBioForm = () => {
             label="Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)} // Use the setter from the context
+            inputProps={{
+              maxLength: 10, // Maximum 10 characters for title
+            }}
           />
+          <Typography variant="body2" color="textSecondary">
+            {`${title.length}/10`}
+          </Typography>
         </FormControl>
       </Box>
       <Box sx={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
@@ -34,7 +41,13 @@ const TitleAndBioForm = () => {
             rows={4}
             value={bio}
             onChange={(e) => setBio(e.target.value)} // Use the setter from the context
+            inputProps={{
+              maxLength: 50, // Maximum 50 characters for bio
+            }}
           />
+          <Typography variant="body2" color="textSecondary">
+            {`${bio.length}/50`}
+          </Typography>
         </FormControl>
       </Box>
       <Button
