811e0462c018af37d12fff4bbd12e64847a7d7a7
harshpathakzz <harshpathak2017@gmail.com>
2023-07-30 12:13:44 +0530

    updated

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getIdByUsername } from "../functions/dbUsernameFunctions";
+import UserProfileAvatar from "../Components/UserProfileAvatar";
 
 const ProfilePage = () => {
   const { username } = useParams();
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const fetchUserId = async () => {
+      const id = await getIdByUsername(username);
+      setUserId(id);
+    };
+    fetchUserId();
+  }, [username]);
+
   return (
     <div>
       <h1>Profile Page</h1>
-      <h2>{username}</h2>
+      {userId ? (
+        <>
+          <h2>Username: {username}</h2>
+          <UserProfileAvatar userId={userId} />
+          <h2>UserId: {userId}</h2>
+        </>
+      ) : (
+        <h2>User not found.</h2>
+      )}
     </div>
   );
 };
