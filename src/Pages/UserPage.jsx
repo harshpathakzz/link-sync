0d59f0f94cf64ce54692f760a134166377e2a874
harshpathakzz <harshpathak2017@gmail.com>
2023-07-30 13:09:22 +0530

    updated

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,33 +1,63 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getIdByUsername } from "../functions/dbUsernameFunctions";
+import { getUserTitleAndBio } from "../functions/dbTitleBioFunctions";
+import { getLinksByUserId } from "../functions/dbLinksFunctions";
 import UserProfileAvatar from "../Components/UserProfileAvatar";
+import Button from "@mui/material/Button";
+import { Typography, Box, Container, Stack } from "@mui/material";
 
 const ProfilePage = () => {
   const { username } = useParams();
   const [userId, setUserId] = useState(null);
+  const [title, setTitle] = useState("");
+  const [bio, setBio] = useState("");
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
-    const fetchUserId = async () => {
+    const fetchUserData = async () => {
       const id = await getIdByUsername(username);
       setUserId(id);
+      const { title, bio } = await getUserTitleAndBio(id);
+      setTitle(title);
+      setBio(bio);
+      const links = await getLinksByUserId(id);
+      setLinks(links);
     };
-    fetchUserId();
+    fetchUserData();
   }, [username]);
 
   return (
-    <div>
-      <h1>Profile Page</h1>
-      {userId ? (
-        <>
-          <h2>Username: {username}</h2>
-          <UserProfileAvatar userId={userId} />
-          <h2>UserId: {userId}</h2>
-        </>
-      ) : (
-        <h2>User not found.</h2>
-      )}
-    </div>
+    <Container maxWidth="md">
+      <Box mt={4} textAlign="center">
+        <UserProfileAvatar userId={userId} />
+        <Typography variant="h4" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {bio}
+        </Typography>
+      </Box>
+      <Box mt={4} textAlign="center">
+        <Stack spacing={2}>
+          {links.map((link) =>
+            link.visibility ? (
+              <Button
+                key={link.id}
+                variant="contained"
+                color="primary"
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                fullWidth
+              >
+                {link.title}
+              </Button>
+            ) : null
+          )}
+        </Stack>
+      </Box>
+    </Container>
   );
 };
 
