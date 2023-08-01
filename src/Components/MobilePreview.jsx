fb06a57aad2e864374b05b6a86a71b2083236e0c
harshpathakzz <harshpathak2017@gmail.com>
2023-08-01 21:25:24 +0530

    updated

--- src/Components/MobilePreview.jsx
+++ src/Components/MobilePreview.jsx
@@ -1,4 +1,11 @@
-import { Box, Button, Container, Typography, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  Stack,
+  Avatar,
+} from "@mui/material";
 import { useTitleAndBioContext } from "../context/TitleAndBioContext";
 import { useLinkContext } from "../context/LinkContext";
 
@@ -6,9 +13,47 @@ const MobilePreview = () => {
   const { username, title, bio, profilePicURL } = useTitleAndBioContext();
   const { links } = useLinkContext();
 
-  const phoneFrameStyles = {
-    width: "280px",
-    height: "500px",
+  const pageStyles = {
+    glassMorphism: true,
+
+    boxContainer: {
+      justifyContent: "center",
+      minHeight: "100vh",
+      backgroundImage:
+        "url('https://firebasestorage.googleapis.com/v0/b/link-sync-64286.appspot.com/o/images%2Fpexels-pixabay-36717.jpg?alt=media&token=56b86be4-efbc-407a-a358-97b3008ab8d0')",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center",
+    },
+    container: (glassMorphism) => ({
+      maxWidth: "md",
+      textAlign: "center",
+      height: "100vh",
+      width: "100%",
+      color: "#fff",
+      padding: "20px",
+      // Set the glass morphism styles conditionally
+      boxShadow: glassMorphism
+        ? "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )"
+        : "none",
+      backdropFilter: glassMorphism ? "blur(15px)" : "none",
+      borderRadius: glassMorphism
+        ? "1px solid rgba( 255, 255, 255, 0.18 )"
+        : "none",
+    }),
+    button: {
+      width: "85%",
+      background: "rgba( 14, 8, 8, 0.8)",
+      boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+      backdropFilter: "blur(14px)",
+      borderRadius: "40px",
+      border: "1px solid rgba( 255, 255, 255, 0.18 )",
+      padding: "10px",
+      color: "#fff",
+      "&:hover": {
+        background: "rgba( 14, 8, 8, 0.8)",
+      },
+    },
   };
 
   return (
@@ -18,34 +63,30 @@ const MobilePreview = () => {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        minHeight: "100vh",
-        border: "1px solid #fff",
+        marginTop: "10vh",
       }}
     >
-      <Box sx={phoneFrameStyles}>
-        <Box
-          sx={{
-            width: "100%",
-            height: "100%",
-            backgroundImage:
-              "url('https://firebasestorage.googleapis.com/v0/b/link-sync-64286.appspot.com/o/images%2Fpexels-pixabay-36717.jpg?alt=media&token=56b86be4-efbc-407a-a358-97b3008ab8d0')",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-            boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-            backdropFilter: "blur(15px)",
-            borderRadius: "1px solid rgba( 255, 255, 255, 0.18 )",
-            padding: "20px",
-          }}
-        >
+      <Box
+        sx={{
+          height: "80vh",
+          width: "250px",
+          overflow: "scroll",
+          border: "12px solid black",
+          borderRadius: "20px",
+        }}
+      >
+        <Box sx={pageStyles.boxContainer}>
           <Container
             maxWidth="md"
-            sx={{
-              textAlign: "center",
-              color: "#fff",
-            }}
+            style={pageStyles.container(pageStyles.glassMorphism)} // Apply the glassMorphism styles here
           >
-            {/* <UserProfileAvatar userId={userId} /> */}
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Avatar
+                alt="User Profile Picture"
+                src={profilePicURL}
+                sx={{ width: 50, height: 50 }}
+              />
+            </Box>
             <Typography variant="h4" gutterBottom>
               {title}
             </Typography>
@@ -64,19 +105,7 @@ const MobilePreview = () => {
                         href={link.url}
                         target="_blank"
                         rel="noopener noreferrer"
-                        sx={{
-                          width: "85%",
-                          background: "rgba( 14, 8, 8, 0.8)",
-                          boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-                          backdropFilter: "blur(14px)",
-                          borderRadius: "40px",
-                          border: "1px solid rgba( 255, 255, 255, 0.18 )",
-                          padding: "10px",
-                          color: "#fff",
-                          "&:hover": {
-                            background: "rgba( 14, 8, 8, 0.8)",
-                          },
-                        }}
+                        sx={pageStyles.button}
                       >
                         {link.title}
                       </Button>
@@ -91,7 +120,7 @@ const MobilePreview = () => {
         <Button
           variant="contained"
           color="primary"
-          sx={{ marginBottom: 1, borderRadius: 10 }}
+          sx={{ margin: 1, borderRadius: 10 }}
         >
           Launch site
         </Button>
