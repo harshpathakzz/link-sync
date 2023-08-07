de24f41a28c353d74d89c9ac701082c922a6f5a8
harshpathakzz <harshpathak2017@gmail.com>
2023-08-07 22:00:57 +0530

    updated

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -18,55 +18,33 @@ const LandingPage = () => {
   }, [isLoggedIn, navigate]);
 
   const cardStyle = {
-    padding: "20px",
-    borderRadius: "8px",
+    padding: "10px",
+    borderRadius: "10px",
     boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
     backgroundImage:
       "linear-gradient(135deg, rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 0.15))",
-    backdropFilter: "blur(10px)",
     display: "flex",
   };
 
   return (
-    <Container maxWidth="md" style={{ textAlign: "center", marginTop: "50px" }}>
-      {/* Login and Signup Buttons */}
-      <Grid
-        container
-        justifyContent="flex-end"
-        style={{ marginBottom: "30px" }}
+    <Container
+      maxWidth="100vw"
+      style={{ textAlign: "center", marginTop: "50px" }}
+    >
+      <Typography
+        variant="h2"
+        component="h1"
+        gutterBottom
+        sx={{ fontWeight: "700" }}
       >
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Login
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            color="secondary"
-            fullWidth
-            onClick={() => {
-              navigate("/signup");
-            }}
-          >
-            Signup
-          </Button>
-        </Grid>
-      </Grid>
-
-      <Typography variant="h4" component="h1" gutterBottom>
         Your Link-Sync
       </Typography>
       <Typography variant="body1" gutterBottom>
         Share multiple links with just one URL!
       </Typography>
+      <Button variant="contained" onClick={() => navigate("/login")}>
+        Get Started
+      </Button>
 
       <Grid
         container
@@ -74,10 +52,16 @@ const LandingPage = () => {
         justifyContent="center"
         style={{ marginTop: "30px" }}
       >
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={7}>
           <Card elevation={3} style={cardStyle}>
-            <Box>
-              <Typography variant="h5" gutterBottom>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h4" gutterBottom>
                 Paste All Links
               </Typography>
               <Typography variant="body2">
@@ -89,11 +73,17 @@ const LandingPage = () => {
           </Card>
         </Grid>
 
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={7}>
           <Card elevation={3} style={cardStyle}>
             <UniqueLinkSVG />
-            <Box>
-              <Typography variant="h5" gutterBottom>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h4" gutterBottom>
                 Get a Unique Link
               </Typography>
               <Typography variant="body2">
@@ -111,27 +101,39 @@ const LandingPage = () => {
         justifyContent="center"
         style={{ marginTop: "30px" }}
       >
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={7}>
           <Card elevation={3} style={cardStyle}>
-            <Box>
-              <Typography variant="h5" gutterBottom>
-                Customizable Templates
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h4" gutterBottom>
+                Analytics
               </Typography>
               <Typography variant="body2">
-                Personalize your Link-Sync with customizable templates to match
-                your style and branding.
+                Get daily, monthly, weakly and lifetime profile vists,
+                performance of every link
               </Typography>
             </Box>
             <AnalyticsSVG />
           </Card>
         </Grid>
 
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={7}>
           <Card elevation={3} style={cardStyle}>
             <CustomDomainSVG />
-            <Box>
-              <Typography variant="h5" gutterBottom>
-                Host on Custom Domain
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h4" gutterBottom>
+                Custom Domain
               </Typography>
               <Typography variant="body2">
                 Take full control by hosting your Link-Sync on your custom
