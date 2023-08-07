fee7ae2d0923bf134293861fe9edda1faf8cde65
harshpathakzz <harshpathak2017@gmail.com>
2023-08-07 20:22:11 +0530

    updated

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
-import { Button, Container, Typography, Grid, Paper } from "@mui/material";
+import { Button, Container, Typography, Grid, Card, Box } from "@mui/material";
 import { useUserAuth } from "../context/UserAuthContext";
 import { useNavigate } from "react-router-dom";
-// import CustomDomainSVG from "../assets/custom-domain.svg";
+import { ReactComponent as CustomDomainSVG } from "../assets/custom-domain.svg";
+import { ReactComponent as ShareAllLinksSVG } from "../assets/share-all.svg";
+import { ReactComponent as AnalyticsSVG } from "../assets/analytics.svg";
+import { ReactComponent as UniqueLinkSVG } from "../assets/unique-link2.svg";
+
 const LandingPage = () => {
   const navigate = useNavigate();
   const { isLoggedIn } = useUserAuth();
@@ -13,22 +17,14 @@ const LandingPage = () => {
     }
   }, [isLoggedIn, navigate]);
 
-  const paperStyle = {
+  const cardStyle = {
     padding: "20px",
     borderRadius: "8px",
     boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
     backgroundImage:
       "linear-gradient(135deg, rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 0.15))",
     backdropFilter: "blur(10px)",
-  };
-
-  const emptyCardStyle = {
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "200px",
-    borderRadius: "8px",
-    border: "2px dashed #ccc",
   };
 
   return (
@@ -72,7 +68,6 @@ const LandingPage = () => {
         Share multiple links with just one URL!
       </Typography>
 
-      {/* Feature: Paste all links */}
       <Grid
         container
         spacing={2}
@@ -80,53 +75,36 @@ const LandingPage = () => {
         style={{ marginTop: "30px" }}
       >
         <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={paperStyle}>
-            <Typography variant="h5" gutterBottom>
-              Feature: Paste All Links
-            </Typography>
-            <Typography variant="body2">
-              Easily paste all your important links in one place and share them
-              with others using just one URL.
-            </Typography>
-          </Paper>
+          <Card elevation={3} style={cardStyle}>
+            <Box>
+              <Typography variant="h5" gutterBottom>
+                Paste All Links
+              </Typography>
+              <Typography variant="body2">
+                Easily paste all your important links in one place and share
+                them with others using just one URL.
+              </Typography>
+            </Box>
+            <ShareAllLinksSVG />
+          </Card>
         </Grid>
 
-        {/* Empty card component for future image */}
         <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={emptyCardStyle}>
-            {/* Add any icon or image placeholder here */}
-          </Paper>
+          <Card elevation={3} style={cardStyle}>
+            <UniqueLinkSVG />
+            <Box>
+              <Typography variant="h5" gutterBottom>
+                Get a Unique Link
+              </Typography>
+              <Typography variant="body2">
+                Your Link-Sync will generate a unique link that combines all
+                your shared links together.
+              </Typography>
+            </Box>
+          </Card>
         </Grid>
       </Grid>
 
-      {/* Feature: Get a unique link */}
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        style={{ marginTop: "30px" }}
-      >
-        {/* Empty card component for future image */}
-        <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={emptyCardStyle}>
-            {/* Add any icon or image placeholder here */}
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={paperStyle}>
-            <Typography variant="h5" gutterBottom>
-              Feature: Get a Unique Link
-            </Typography>
-            <Typography variant="body2">
-              Your LInk-Sync will generate a unique link that combines all your
-              shared links together.
-            </Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-
-      {/* Feature: Customizable templates */}
       <Grid
         container
         spacing={2}
@@ -134,50 +112,33 @@ const LandingPage = () => {
         style={{ marginTop: "30px" }}
       >
         <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={paperStyle}>
-            <Typography variant="h5" gutterBottom>
-              Feature: Customizable Templates
-            </Typography>
-            <Typography variant="body2">
-              Personalize your LInk-Sync with customizable templates to match
-              your style and branding.
-            </Typography>
-          </Paper>
-        </Grid>
-
-        {/* Empty card component for future image */}
-        <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={emptyCardStyle}>
-            {/* Add any icon or image placeholder here */}
-          </Paper>
-        </Grid>
-      </Grid>
-
-      {/* Feature: Host on custom domain */}
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        style={{ marginTop: "30px" }}
-      >
-        {/* Empty card component for future image */}
-        <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={emptyCardStyle}>
-            {/* Add any icon or image placeholder here */}
-            <img src="../assets/analytics.svg" alt="" />
-          </Paper>
+          <Card elevation={3} style={cardStyle}>
+            <Box>
+              <Typography variant="h5" gutterBottom>
+                Customizable Templates
+              </Typography>
+              <Typography variant="body2">
+                Personalize your Link-Sync with customizable templates to match
+                your style and branding.
+              </Typography>
+            </Box>
+            <AnalyticsSVG />
+          </Card>
         </Grid>
 
         <Grid item xs={12} sm={6}>
-          <Paper elevation={3} style={paperStyle}>
-            <Typography variant="h5" gutterBottom>
-              Feature: Host on Custom Domain
-            </Typography>
-            <Typography variant="body2">
-              Take full control by hosting your LInk-Sync on your cu stom domain
-              for a seamless experience.
-            </Typography>
-          </Paper>
+          <Card elevation={3} style={cardStyle}>
+            <CustomDomainSVG />
+            <Box>
+              <Typography variant="h5" gutterBottom>
+                Host on Custom Domain
+              </Typography>
+              <Typography variant="body2">
+                Take full control by hosting your Link-Sync on your custom
+                domain for a seamless experience.
+              </Typography>
+            </Box>
+          </Card>
         </Grid>
       </Grid>
     </Container>
