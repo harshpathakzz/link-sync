8b2a5b2d3294900b51d9ea88deb1737c9425102e
harshpathakzz <harshpathak2017@gmail.com>
2023-08-08 17:47:56 +0530

    updated

--- src/functions/dbFunctions.jsx
+++ src/functions/dbFunctions.jsx
@@ -1,5 +1,12 @@
 import { db } from "../firebase/firebaseConfig";
-import { doc, setDoc, collection } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
 export const addUserToCollection = async (user) => {
   try {
@@ -17,5 +24,27 @@ export const addUserToCollection = async (user) => {
     console.log("User added to collection:", user);
   } catch (error) {
     console.error("Error adding user to collection:", error);
+  }
+};
+
+export const getDataByUsername = async (username) => {
+  try {
+    const usersCollectionRef = collection(db, "users");
+    const q = query(usersCollectionRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      const userData = querySnapshot.docs[0].data();
+      const userId = querySnapshot.docs[0].id;
+      const { title, bio, profilePicUrl } = userData;
+      console.log("User data retrieved:", userData);
+      return { title, bio, profilePicUrl, id: userId };
+    } else {
+      console.log("User not found");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error retrieving user data:", error);
+    throw error;
   }
 };
